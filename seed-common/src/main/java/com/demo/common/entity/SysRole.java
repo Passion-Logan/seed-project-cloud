049ed2e6200a239e8cd39110210871607f799fc4
Passion-Logan <wangdadamailbox@163.com>
2021-07-11 14:39:03 +0800

    添加common包文件

---
+++ seed-common/src/main/java/com/demo/common/entity/SysRole.java
@@ -0,0 +1,60 @@
+package com.demo.common.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @Description: 角色信息表
+ * @date: 2020年06月17日 14:37
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class SysRole implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 角色ID
+     */
+    @TableId(type = IdType.ID_WORKER_STR)
+    private String id;
+    /**
+     * 角色编码
+     */
+    private String roleCode;
+    /**
+     * 角色名称
+     */
+    private String roleName;
+    /**
+     * 角色权限字符串
+     */
+    private String permission;
+    /**
+     * 角色级别
+     */
+    private Integer level;
+    /**
+     * 数据权限
+     */
+    private String dataScope;
+    /**
+     * 创建者
+     */
+    private String createBy;
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+    /**
+     * 备注
+     */
+    private String remark;
+}
---
+++ seed-common/src/main/java/com/demo/common/entity/SysRoleMenu.java
@@ -0,0 +1,28 @@
+package com.demo.common.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * @Description: 角色和菜单关联表
+ * @date: 2020年06月17日 14:57
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class SysRoleMenu implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 菜单ID
+     */
+    private String menuId;
+    /**
+     * 角色ID
+     */
+    private String roleId;
+}
