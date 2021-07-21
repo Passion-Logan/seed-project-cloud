82fcaf5db57bbaaf890ce1ef1abe83319468fb27
Passion-Logan <wangdadamailbox@163.com>
2021-07-21 09:35:40 +0800

    update

--- seed-auth/src/main/java/com/demo/cody/auth/service/security/CustomUserService.java
+++ seed-auth/src/main/java/com/demo/cody/auth/service/security/CustomUserService.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 /**
+ *
  * ClassName: CustomUserService
  *
  * @author WQL
