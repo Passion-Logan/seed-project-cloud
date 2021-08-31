a084b3cdc986602c6dc39e18467a8000e6c903e0
Passion-Logan <wangdadamailbox@163.com>
2021-08-31 18:05:51 +0800

    style:排除security依赖

--- seed-service/seed-system/src/main/java/com/demo/cody/system/SeedSystemApplication.java
+++ seed-service/seed-system/src/main/java/com/demo/cody/system/SeedSystemApplication.java
@@ -2,8 +2,6 @@ package com.demo.cody.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 /**
  * ClassName: GatewayApplication
